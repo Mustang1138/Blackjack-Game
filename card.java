f7bd654b56a3fd64c6fef24b5d12d806684cb799
Joshua Bunday <93491336+bunday1@users.noreply.github.com>
2023-03-27 10:51:56 +0100

    Update card.java
    Added references

--- card.java
+++ card.java
@@ -17,11 +17,14 @@ public class card {
 	
 	public void generate() {
 		/* This method generates a card by picking a random index from a list of cards.
-		 * The list is retrieved from the deck object.*/
+		 * The list is retrieved from the deck object.
+		 * JAVATPOINT, 2023. Java List[online]. Available from: https://www.javatpoint.com/java-list [Accessed 20 March 2023]*/
+
 		List<String> deckList = deck.getDeck();
 		List<Integer> deckNums = deck.getDeckNums();
 		/*Random integer generated between 0 and the length of the deck,
-		 * which begins at 52 and is decreased by 1 every time a card is chosen*/
+		 * which begins at 52 and is decreased by 1 every time a card is chosen
+		 * EDUCATIVE, 2023. How to generate random numbers in Java[online]. Available from: https://www.educative.io/answers/how-to-generate-random-numbers-in-java [Accessed 20 March 2023]*/
 		Random rand = new Random();
 		var num = rand.nextInt(deckList.size());
 		cardName = deckList.get(num); //Card name chosen from list of card names
